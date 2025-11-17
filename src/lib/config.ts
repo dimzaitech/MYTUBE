a45adb3aae6ac0668dc4ca93089ea9f4bede8fb2
Yanwar Ananda <yanwarananda@gmail.com>
2025-11-17 07:02:51 +0000

    1. SETUP API KEY & CONFIG File: firebase.js atau config.js
    javascript
    /

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+// Konfigurasi YouTube API
+const YOUTUBE_API_KEY = 'YOUR_YOUTUBE_API_KEY_HERE';
+const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3';
+
+export { YOUTUBE_API_KEY, YOUTUBE_API_URL };
